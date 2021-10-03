5f47fcdcc9eca084aff1f0070cf9369c8ca5570a
eliteslayer10 <83776206+eliteslayer10@users.noreply.github.com>
2021-10-03 18:19:54 +0530

    React Chatting App

---
+++ Awesome Javascript Projects_Scripts/chat-app-react-master/src/App.js
@@ -0,0 +1,30 @@
+  
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = 'ef7c8d44-f431-4080-bbf6-a075f9702743';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
+
+export default App;
+
+
+
