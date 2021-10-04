9252fbb3adae459af7eb4f7973a5808b445d3763
mahitha2001 <mahithamilli@gmail.com>
2021-10-04 19:08:06 +0530

    digital clock

---
+++ Awesome Javascript Projects_Scripts/digital-clock/clock.js
@@ -0,0 +1,17 @@
+const clock = document.querySelector('.clock');
+
+const tick = () => {
+  const now = new Date();
+  const h = now.getHours();
+  const m = now.getMinutes();
+  const s = now.getSeconds();
+  const html = `
+    <span>${h}</span> : 
+    <span>${m}</span> : 
+    <span>${s}</span>
+  `;
+  clock.innerHTML = html;
+
+}
+
+setInterval(tick, 1000);
